9d89c2721c5a318fc6f6902bbf780a7ffe1f1f29
Stephen Smyth <kaheglar@gmail.com>
2020-09-19 21:36:58 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,13 +6,13 @@ const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  'index.html',
-  './', // Alias for index.html
-  'manifest.json',
-  'css/style.css',
-  'img/icon-192x192.png',
-  'img/icon-512x512.png',
-  'js/main.js'
+  //'index.html',
+  //'./', // Alias for index.html
+  //'manifest.json',
+  //'css/style.css',
+  //'img/icon-192x192.png',
+  //'img/icon-512x512.png',
+  //'js/main.js'
 ];
 
 // The install handler takes care of precaching the resources we always need.
